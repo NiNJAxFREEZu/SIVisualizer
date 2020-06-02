de93a8a907d936d32a87f64c59dab9e464c710af
Marcin Borysiewicz <frugoxmaniak@gmail.com>
2020-06-02 18:16:22 +0000

    reading board file char by char

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def drawBoard(board):
     blockSize = int(Config.Window.side / len(board))  # Set the size of the grid block
     for x in range(len(board)):
         for y in range(len(board)):
+            char = board[x][y]
+            print(char)
             rect = pygame.Rect(x * blockSize, y * blockSize,
                                blockSize, blockSize)
             pygame.draw.rect(SCREEN, Config.Grid.colour, rect, Config.Grid.thickness)
@@ -52,11 +54,8 @@ pygame.display.set_caption(Config.Window.caption)
 icon = pygame.image.load(Config.Window.iconPath)
 pygame.display.set_icon(icon)
 
-# TEST
 BOARD = board.load("board_01.txt")
 RESULT = result.load("result_01.txt")
-print(BOARD)
-print(RESULT)
 
 # Action!
 SCREEN.fill(Config.Window.backgroundColour)
